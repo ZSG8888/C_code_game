065e8d04fd233a96be21f9361af49c0e6d4f1197
zheng-shaoguang <1085229072@qq.com>
2023-04-08 00:02:09 +0800

    测试githup

---
+++ test_4.3/test_4.3/test_4.3.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main
+{
+
+
+	//int main()
+//{
+//	char arr[10] = { 0 };
+//	int i = 0;
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	scanf("%", &arr[i]);
+//	//}
+//	//for (i = 0; i < 10; i++)
+//	//{
+//	//	printf("%d", arr[i]);
+//	//}
+//	scanf("%s", arr);
+//	printf("%s", arr);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("µÚÒ»´Î:%d%d\n", a, b);
+//
+//	scanf("%d%d", &a, &b);
+//	printf("%d%d\n", a, b);
+//	return 0;
+//}
+}
